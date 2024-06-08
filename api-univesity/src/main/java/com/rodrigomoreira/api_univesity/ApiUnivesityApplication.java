0a8a7c51351aa5c69dd29909aad0eb916cc7a4b0
Rodrigo Moreira <rodrigomoreir4@gmail.com>
2024-06-08 12:57:26 -0300

    Project created

---
+++ api-univesity/src/main/java/com/rodrigomoreira/api_univesity/ApiUnivesityApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigomoreira.api_univesity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUnivesityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUnivesityApplication.class, args);
+	}
+
+}
