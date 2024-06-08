cae2afbdd952cd3c22d0aa8473c07df935bef663
Rodrigo Moreira <rodrigomoreir4@gmail.com>
2024-06-08 13:49:05 -0300

    Services created

---
+++ api-univesity/src/main/java/com/rodrigomoreira/api_univesity/services/CourseService.java
@@ -0,0 +1,43 @@
+package com.rodrigomoreira.api_univesity.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigomoreira.api_univesity.domain.courses.Course;
+import com.rodrigomoreira.api_univesity.domain.users.User;
+import com.rodrigomoreira.api_univesity.repositories.CourseRepository;
+
+@Service
+public class CourseService {
+    
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public Course findCourseById(Long id) throws Exception {
+        return courseRepository.findCourseById(id).orElseThrow(() -> new Exception("Course not found"));
+    }
+
+    public Course findCourseByName(String name) throws Exception{
+        return courseRepository.findCourseByName(name).orElseThrow(() -> new Exception("Course not found"));
+    }
+    
+    public Course createCourse(Course course) throws Exception {
+        course.setName(course.getName().toLowerCase());
+        return courseRepository.save(course);
+    }
+
+    public List<Course> getAllCourses(){
+        return courseRepository.findAll();
+    }
+
+    public void deleteCourseById(Long id) throws Exception{
+        Course course = findCourseById(id);
+        for (User user : course.getUsers()){
+            user.getCourses().remove(course);
+        }
+        courseRepository.deleteById(course.getId());
+    }
+    
+}
